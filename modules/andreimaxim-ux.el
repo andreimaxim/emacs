9417b07e14bb5194c626612cacf6b748a3873794
Andrei Maxim <andrei@andreimaxim.ro>
2022-12-28 23:27:30 +0200

    Set the correct font size on Windows and Linux

--- modules/andreimaxim-ux.el
+++ modules/andreimaxim-ux.el
@@ -9,10 +9,10 @@
 (set-face-background 'fringe (face-attribute 'default :background))
 
 (set-face-foreground 'line-number "gray90")
-(set-face-attribute 'default nil :family "PragmataPro Mono Liga" :height 160)
+(set-face-attribute 'default nil :family "PragmataPro Mono Liga" :height 120)
 (set-face-attribute 'fixed-pitch nil :font "PragmataPro Mono Liga")
 (set-face-attribute 'variable-pitch nil :family "PragmataPro Mono Liga")
-(set-face-attribute 'mode-line nil :height 160)
+(set-face-attribute 'mode-line nil :height 120)
 
 ;; Highlight the current line
 (use-package hl-line
