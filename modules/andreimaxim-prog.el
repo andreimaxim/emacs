bbc022fc608f6ada61ec01b80532f2768e2682c8
Andrei Maxim <andrei@andreimaxim.ro>
2023-05-18 09:44:09 +0300

    I don't really use the align feature

--- modules/andreimaxim-prog.el
+++ modules/andreimaxim-prog.el
@@ -77,42 +77,4 @@
 (use-package markdown-mode
   :ensure t)
 
-;; For UUIDv4 generation
-(use-package uuidgen
-  :ensure t)
-
-(use-package align
-	:ensure nil
-	:config
-	(add-to-list 'align-rules-list
-							 '(ruby-comma-delimiter
-								 (regexp . ",\\(\\s-*\\)[^# \t\n]")
-								 (repeat . t)
-								 (modes  . '(ruby-mode))))
-
-	(add-to-list 'align-rules-list
-							 '(ruby-hash-literal
-								 (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
-								 (group 2 3)
-								 (repeat . t)
-								 (modes  . '(ruby-mode))))
-
-	(add-to-list 'align-rules-list
-							 '(ruby-hash-literal2
-								 (regexp . "[a-z0-9]:\\(\\s-*\\)[^# \t\n]")
-								 (repeat . t)
-								 (modes  . '(ruby-mode))))
-
-	(add-to-list 'align-rules-list
-							 '(ruby-assignment-literal
-								 (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
-								 (repeat . t)
-								 (modes  . '(ruby-mode))))
-
-	(add-to-list 'align-rules-list
-							 '(ruby-xmpfilter-mark
-								 (regexp . "\\(\\s-*\\)# => [^#\t\n]")
-								 (repeat . nil)
-								 (modes  . '(ruby-mode)))))
-
 (provide 'andreimaxim-prog)
