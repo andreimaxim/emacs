f774d94de4c951aaaccc1c43e8f861c8111cb79c
Andrei Maxim <andrei@andreimaxim.ro>
2023-10-31 18:30:13 +0200

    Try to use align with the tree-sitter mode

--- modules/andreimaxim-ruby-align.el
+++ modules/andreimaxim-ruby-align.el
@@ -24,32 +24,32 @@
              '(ruby-comma-delimiter
                (regexp . ",\\(\\s-*\\)[^# \t\n]")
                (repeat . t)
-               (modes  . '(ruby-mode))))
+               (modes  . '(ruby-ts-mode))))
 
 (add-to-list 'align-rules-list
              '(ruby-hash-literal
                (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
                (group 2 3)
                (repeat . t)
-               (modes  . '(ruby-mode))))
+               (modes  . '(ruby-ts-mode))))
 
 (add-to-list 'align-rules-list
              '(ruby-hash-literal2
                (regexp . "[a-z0-9]:\\(\\s-*\\)[^# \t\n]")
                (repeat . t)
-               (modes  . '(ruby-mode))))
+               (modes  . '(ruby-ts-mode))))
 
 (add-to-list 'align-rules-list
              '(ruby-assignment-literal
                (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
                (repeat . t)
-               (modes  . '(ruby-mode))))
+               (modes  . '(ruby-ts-mode))))
 
 (add-to-list 'align-rules-list
              '(ruby-xmpfilter-mark
                (regexp . "\\(\\s-*\\)# => [^#\t\n]")
                (repeat . nil)
-               (modes  . '(ruby-mode))))
+               (modes  . '(ruby-ts-mode))))
 
 
 (provide 'andreimaxim-ruby-align)
